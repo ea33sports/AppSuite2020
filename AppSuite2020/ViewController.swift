880820555cd299953ec29c0481d459da963be855
Eric Andersen <ea33sports@me.com>
2020-03-15 16:59:18 -0500

    Initial Commit

---
+++ AppSuite2020/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppSuite2020
+//
+//  Created by Eric Andersen on 3/15/20.
+//  Copyright © 2020 Eric Andersen. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
