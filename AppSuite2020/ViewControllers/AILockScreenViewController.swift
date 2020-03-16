2617bcb219224e1be8fa9d39a1cece24259ba3de
Eric Andersen <ea33sports@me.com>
2020-03-15 21:54:14 -0500

    Skeletal Commit

---
+++ AppSuite2020/ViewControllers/AILockScreenViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AILockScreenViewController.swift
+//  AppSuite2020
+//
+//  Created by Eric Andersen on 3/15/20.
+//  Copyright © 2020 Eric Andersen. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class AILockScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AppSuite2020/ViewControllers/SimpleSuiteViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SimpleSuiteViewController.swift
+//  AppSuite2020
+//
+//  Created by Eric Andersen on 3/15/20.
+//  Copyright © 2020 Eric Andersen. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class SimpleSuiteViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        // Load the "Box" scene from the "Experience" Reality File
+//        let boxAnchor = try! Experience.loadBox()
+//        
+//        // Add the box anchor to the scene
+//        arView.scene.anchors.append(boxAnchor)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
